9157eb346d4c193b3788a85f3fc48b633dab4a8c
Henefisa <cherishnghia@gmail.com>
2021-06-19 17:19:20 +0700

    mock my word page

---
+++ app/src/main/java/com/xfs/flashcard/models/Word.kt
@@ -0,0 +1,11 @@
+package com.xfs.flashcard.models
+
+data class Word(
+    val id: String,
+    val value: String,
+    val mean: String,
+    val spell: String,
+    val examples: List<String>
+) {
+
+}
