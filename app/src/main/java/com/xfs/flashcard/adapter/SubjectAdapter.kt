dad310131e1813780993c767aa97d25e9b70abb2
nguyen suri <suringuyen2409@gmail.com>
2021-06-25 16:27:41 +0700

    add Flashcard

--- app/src/main/java/com/xfs/flashcard/adapter/SubjectAdapter.kt
+++ app/src/main/java/com/xfs/flashcard/adapter/SubjectAdapter.kt
@@ -1,12 +1,16 @@
 package com.xfs.flashcard.adapter
 
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import com.squareup.picasso.Picasso
 import com.xfs.flashcard.R
+import com.xfs.flashcard.activities.AboutActivity
+import com.xfs.flashcard.activities.Flashcard
 import com.xfs.flashcard.models.Subject
 import java.util.*
 
@@ -33,7 +37,6 @@ class SubjectAdapter(a: ArrayList<Subject>) : BaseAdapter() {
         val subject = getItem(position)
         view.findViewById<TextView>(R.id.nameSubj).text = subject.name
         Picasso.get().load(subject.image).placeholder(R.drawable.icon_img).into(view.findViewById<ImageView>(R.id.imgSubj))
-
         return view
     }
 }
