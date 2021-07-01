4ab4e43eaa4929cb381d2e84ac1e36853c9ed810
Henefisa <cherishnghia@gmail.com>
2021-07-01 19:25:02 +0700

    update my words

--- app/src/main/java/com/xfs/flashcard/activities/MyWordActivity.kt
+++ app/src/main/java/com/xfs/flashcard/activities/MyWordActivity.kt
@@ -1,10 +1,10 @@
 package com.xfs.flashcard.activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.Settings
-import android.util.Log
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.ktx.firestore
@@ -16,37 +16,39 @@ import com.xfs.flashcard.models.Word
 class MyWordActivity : AppCompatActivity() {
     lateinit var myWordListView: ListView
     private val db = Firebase.firestore
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.my_word_list_view)
-        Log.d("SAMPLE", "RUNNNNN")
         val id: String = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
-
         db.collection("Favorites").document(id).get().addOnSuccessListener { docs ->
-            Log.d("SAMPLE", docs.toString())
             if (docs.exists()) {
                 val words = ArrayList<Word>()
                 val myWordAdapter = MyWordAdapter(words)
-                val ws = docs.data?.get("words") as ArrayList<String>
-                db.collection("Words").whereIn(FieldPath.documentId(), ws).get().addOnSuccessListener { docs ->
-                    if (!docs.isEmpty) {
-                        for (doc in docs) {
-                            val data = doc.data
-                            val word = Word(
-                                doc.id,
-                                data["value"] as String,
-                                data["mean"] as String,
-                                data["spell"] as String,
-                                data["image"] as String,
-                                data["examples"] as ArrayList<String>
-                            )
-                            words.add(word)
+                val ws = docs.data?.get("words")
+                if (ws != null) {
+                    db.collection("Words").whereIn(FieldPath.documentId(), ws as ArrayList<String>).get().addOnSuccessListener { docs ->
+                        if (!docs.isEmpty) {
+                            for (doc in docs) {
+                                val data = doc.data
+                                val word = Word(
+                                    doc.id,
+                                    data["value"] as String,
+                                    data["mean"] as String,
+                                    data["spell"] as String,
+                                    data["image"] as String,
+                                    data["examples"] as ArrayList<String>
+                                )
+                                words.add(word)
+                            }
+                            myWordListView = findViewById(R.id.my_words_list)
+                            myWordListView.adapter = myWordAdapter
                         }
-                        myWordAdapter.notifyDataSetChanged()
-                        myWordListView = findViewById(R.id.my_words_list)
-                        myWordListView.adapter = myWordAdapter
                     }
+                } else {
+                    Toast.makeText(this, "Your favorite words is empty!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, HomepageActivity::class.java)
+                    startActivity(intent)
+                    finish()
                 }
             }
         }
