db207fcc46a8a68f7434a46f7e8db427feb9ca69
Henefisa <cherishnghia@gmail.com>
2021-07-11 13:05:37 +0700

    shuffle list

--- app/src/main/java/com/xfs/flashcard/activities/QuizActivity.kt
+++ app/src/main/java/com/xfs/flashcard/activities/QuizActivity.kt
@@ -55,6 +55,7 @@ class QuizActivity : AppCompatActivity(), View.OnClickListener {
                     )
 
                 }
+                questionList.shuffle()
                 setQuiz()
             }
         }
